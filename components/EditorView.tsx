bdd8611f91a937bdf90475c5e005ad1eb0278fa0
Takuma Kira <takuma@kirakiraworx.com>
2022-05-01 01:08:32 +0900

    [dev] Build MainView

--- components/EditorView.tsx
+++ components/EditorView.tsx
@@ -1,8 +1,14 @@
-import { View, Text } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
 
 const EditorView = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>EditorView</Text>
     </View>
   )
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -5,10 +5,8 @@ export const SIDEBAR_WIDTH = 250
 const styles = StyleSheet.create({
   container: {
     width: SIDEBAR_WIDTH,
-    height: '100%',
+    flex: 1,
     backgroundColor: '#1D1F22',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   text: {
     color: '#FFFFFF',
