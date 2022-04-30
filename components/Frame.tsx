4f88309fc915f63aaae044e90a55946c0bf376d4
Takuma Kira <takuma@kirakiraworx.com>
2022-04-30 20:17:43 +0900

    [dev] Match the animation to the design

--- components/Frame.tsx
+++ components/Frame.tsx
@@ -1,26 +1,38 @@
 import React, { Dispatch, SetStateAction } from 'react'
-import { Animated, StyleSheet, View } from 'react-native'
+import { Animated, StyleSheet, View, Dimensions } from 'react-native'
 
 const SIDEBAR_WIDTH = 250
-const ANIM_DURATION = 1000
+const ANIM_DURATION = 500
 
 const Frame = (props: {sidebar: () => JSX.Element, main: (setShowSidebar: Dispatch<SetStateAction<boolean>>) => JSX.Element}) => {
   const {
     sidebar, main
   } = props
   const [showSidebar, setShowSidebar] = React.useState(false)
-  const widthAnim = React.useRef(new Animated.Value(0)).current
+  const windowWidth = Dimensions.get('window').width
+  const containerWidthAnim = React.useRef(new Animated.Value(windowWidth)).current
+  const sidebarWidthAnim = React.useRef(new Animated.Value(0)).current
 
   const showAnim = () => {
-    Animated.timing(widthAnim, {
+    Animated.timing(sidebarWidthAnim, {
       toValue: SIDEBAR_WIDTH,
+      duration: ANIM_DURATION,
+      useNativeDriver: true
+    }).start()
+    Animated.timing(containerWidthAnim, {
+      toValue: windowWidth + SIDEBAR_WIDTH,
       duration: ANIM_DURATION,
       useNativeDriver: true
     }).start()
   }
   const hideAnim = () => {
-    Animated.timing(widthAnim, {
+    Animated.timing(sidebarWidthAnim, {
       toValue: 0,
+      duration: ANIM_DURATION,
+      useNativeDriver: true
+    }).start()
+    Animated.timing(containerWidthAnim, {
+      toValue: windowWidth,
       duration: ANIM_DURATION,
       useNativeDriver: true
     }).start()
@@ -31,14 +43,14 @@ const Frame = (props: {sidebar: () => JSX.Element, main: (setShowSidebar: Dispat
   }, [showSidebar])
 
   return (
-    <View style={styles.container}>
-      <Animated.View style={[styles.sidebarContainer, {width: widthAnim}]}>
+    <Animated.View style={[styles.container, {width: containerWidthAnim}]}>
+      <Animated.View style={[styles.sidebarContainer, {width: sidebarWidthAnim}]}>
         {sidebar()}
       </Animated.View>
       <View style={styles.mainContainer}>
         {main(setShowSidebar)}
       </View>
-    </View>
+    </Animated.View>
   )
 }
 
@@ -52,6 +64,7 @@ const styles = StyleSheet.create({
   sidebarContainer: {
     height: '100%',
     overflow: 'hidden',
+    alignItems: 'flex-end',
   },
   mainContainer: {
     flex: 1,
