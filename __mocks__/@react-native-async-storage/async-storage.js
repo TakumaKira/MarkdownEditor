3972a0c0a14669681b9b6a723fe289fa9b37b0be
Takuma Kira <takuma@kirakiraworx.com>
2022-06-16 10:02:14 +0900

    [fix] Fix ';' expected error

--- __mocks__/@react-native-async-storage/async-storage.js
+++ __mocks__/@react-native-async-storage/async-storage.js
@@ -1,1 +1,2 @@
-export default from '@react-native-async-storage/async-storage/jest/async-storage-mock'
+import * as AsyncStorageMock from '@react-native-async-storage/async-storage/jest/async-storage-mock'
+export default AsyncStorageMock
