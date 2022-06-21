f3aa914bff906507bc7e8ba61ddba9c1fc1c664d
Takuma Kira <takuma@kirakiraworx.com>
2022-06-21 09:58:50 +0900

    [refactor] Refactor Confirmation stories using decorator

--- storybook/stories/Confirmation.stories.tsx
+++ storybook/stories/Confirmation.stories.tsx
@@ -16,63 +16,44 @@ import ThemeWrapper from '../utils/ThemeWrapper';
 const mockStore = getMockStore()
 
 storiesOf('Confirmation', module)
-  .add('Delete', () =>
+  .addDecorator(story =>
     <Provider store={mockStore}>
       <ThemeWrapper isDark={boolean('dark mode', false)}>
         {colorScheme =>
           <View style={utilStyles.fullscreen}>
             <MockText colorScheme={colorScheme} />
             {boolean('show', true) &&
-              <Confirmation
-                title={text('title', confirmationMessages[ConfirmationState.DELETE].title)}
-                message={text('message', confirmationMessages[ConfirmationState.DELETE].message('Document Title.md'))}
-                buttonLabel={text('button label', confirmationMessages[ConfirmationState.DELETE].buttonLabel).replace('&amp;', '&')}
-                onPressButton={action('onPressButton')}
-                onPressBackground={action('onPressBackground')}
-              />
+              <>{story()}</>
             }
           </View>
         }
       </ThemeWrapper>
     </Provider>
+  )
+  .add('Delete', () =>
+    <Confirmation
+      title={text('title', confirmationMessages[ConfirmationState.DELETE].title)}
+      message={text('message', confirmationMessages[ConfirmationState.DELETE].message('Document Title.md'))}
+      buttonLabel={text('button label', confirmationMessages[ConfirmationState.DELETE].buttonLabel).replace('&amp;', '&')}
+      onPressButton={action('onPressButton')}
+      onPressBackground={action('onPressBackground')}
+    />
   )
   .add('Leaved unsaved document', () =>
-    <Provider store={mockStore}>
-      <ThemeWrapper isDark={boolean('dark mode', false)}>
-        {colorScheme =>
-          <View style={utilStyles.fullscreen}>
-            <MockText colorScheme={colorScheme} />
-            {boolean('show', true) &&
-              <Confirmation
-                title={text('title', confirmationMessages[ConfirmationState.LEAVE_UNSAVED_DOCUMENT].title)}
-                message={text('message', confirmationMessages[ConfirmationState.LEAVE_UNSAVED_DOCUMENT].message('Document Title.md'))}
-                buttonLabel={text('button label', confirmationMessages[ConfirmationState.LEAVE_UNSAVED_DOCUMENT].buttonLabel).replace('&amp;', '&')}
-                onPressButton={action('onPressButton')}
-                onPressBackground={action('onPressBackground')}
-              />
-            }
-          </View>
-        }
-      </ThemeWrapper>
-    </Provider>
+    <Confirmation
+      title={text('title', confirmationMessages[ConfirmationState.LEAVE_UNSAVED_DOCUMENT].title)}
+      message={text('message', confirmationMessages[ConfirmationState.LEAVE_UNSAVED_DOCUMENT].message('Document Title.md'))}
+      buttonLabel={text('button label', confirmationMessages[ConfirmationState.LEAVE_UNSAVED_DOCUMENT].buttonLabel).replace('&amp;', '&')}
+      onPressButton={action('onPressButton')}
+      onPressBackground={action('onPressBackground')}
+    />
   )
   .add('Long text', () =>
-    <Provider store={mockStore}>
-      <ThemeWrapper isDark={boolean('dark mode', false)}>
-        {colorScheme =>
-          <View style={utilStyles.fullscreen}>
-            <MockText colorScheme={colorScheme} />
-            {boolean('show', true) &&
-              <Confirmation
-                title={text('title', LONG_TITLE)}
-                message={text('message', LONG_TEXT)}
-                buttonLabel={text('button label', 'Click')}
-                onPressButton={action('onPressButton')}
-                onPressBackground={action('onPressBackground')}
-              />
-            }
-          </View>
-        }
-      </ThemeWrapper>
-    </Provider>
+    <Confirmation
+      title={text('title', LONG_TITLE)}
+      message={text('message', LONG_TEXT)}
+      buttonLabel={text('button label', 'Click')}
+      onPressButton={action('onPressButton')}
+      onPressBackground={action('onPressBackground')}
+    />
   )
