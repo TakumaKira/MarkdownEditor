1660474079f746afe5ce1c922870cf4a20ba9d74
Takuma Kira <takuma@kirakiraworx.com>
2022-11-21 22:28:00 +0900

    [refactor] Modify api integration testing environment

--- api/tests/integration/auth.test.ts
+++ api/tests/integration/auth.test.ts
@@ -2,14 +2,6 @@ import request from 'supertest'
 import { API_PATHS } from '../../src/constants'
 import apiApp, { wsServer } from '../../src/servers/api'
 
-jest.mock('../../src/getEnvs', () => ({
-  WS_PORT: 3001, // TODO: Mock WS_PORT should not be set here.
-  DATABASE_HOST: 'db',
-  MYSQL_DATABASE: 'markdown_editor_auto_test_db',
-  MYSQL_DATABASE_USERNAME_FOR_APP: 'markdown_editor_app_auto_test',
-  MYSQL_DATABASE_PASSWORD_FOR_APP: 'password_for_app_auto_test',
-}))
-
 describe(`POST ${API_PATHS.AUTH.LOGIN.path}`, () => {
   afterEach(() => {
     wsServer.close()
