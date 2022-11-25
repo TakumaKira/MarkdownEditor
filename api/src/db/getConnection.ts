2cc3927949708d4b59f0569501d70580ebc185dc
Takuma Kira <takuma@kirakiraworx.com>
2022-11-25 15:18:09 +0900

    [fix] Fix connection between api and db

--- api/src/db/getConnection.ts
+++ api/src/db/getConnection.ts
@@ -4,9 +4,9 @@ import { DATABASE_HOST, MYSQL_DATABASE, MYSQL_PASSWORD, MYSQL_USER } from '../ge
 const getConnection = () => {
   return mysql.createConnection({
     host     : DATABASE_HOST,
+    database : MYSQL_DATABASE,
     user     : MYSQL_USER,
     password : MYSQL_PASSWORD,
-    database : MYSQL_DATABASE
   })
 }
 export default getConnection
