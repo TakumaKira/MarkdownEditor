00118a9512d977c82c74ea59c4976603670f1b3d
Takuma Kira <takuma@kirakiraworx.com>
2023-02-23 01:08:52 +0900

    [refactor] Add E2E tests for reset password

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -1,13 +1,14 @@
-const testEmail = `test@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
+const testEmail1 = `test1@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
 const testEmail2 = `test2@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
-const testPassword = 'test1234'
+const testPassword1 = 'test1234'
 const testPassword2 = 'test5678'
 const testPassword3 = 'test9012'
+const testPassword4 = 'test3456'
 
 describe('auth' , () => {
   context('singup', () => {
     before(() => {
-      cy.task('clearUser', testEmail)
+      cy.task('clearUser', testEmail1)
       cy.clearLocalStorageSnapshot()
     })
     beforeEach(() => {
@@ -24,14 +25,14 @@ describe('auth' , () => {
       cy.visit('/')
       cy.getBySel('topbar-menu-button').click()
       cy.getBySel('sidebar-signup-button').click()
-      cy.getBySel('auth-modal-email-input').type(testEmail)
-      cy.getBySel('auth-modal-password-input').type(testPassword)
-      cy.getBySel('auth-modal-password-confirm-input').type(testPassword)
+      cy.getBySel('auth-modal-email-input').type(testEmail1)
+      cy.getBySel('auth-modal-password-input').type(testPassword1)
+      cy.getBySel('auth-modal-password-confirm-input').type(testPassword1)
       cy.getBySel('auth-modal-submit-button').click()
-      cy.contains(`Confirmation email was sent to ${testEmail}.`)
+      cy.contains(`Confirmation email was sent to ${testEmail1}.`)
       cy.mailosaurGetMessage(
         Cypress.env('MAILOSAUR_SERVER_ID'),
-        {sentTo: testEmail},
+        {sentTo: testEmail1},
         {receivedAfter: signupTestStart}
       ).then(message => {
         expect(message.html.links[0].href).to.equal(message.text.links[0].href)
@@ -44,7 +45,7 @@ describe('auth' , () => {
       cy.contains('Your email is confirmed successfully.')
       cy.getBySel('auth-modal-ok-button').click()
       cy.getBySel('topbar-menu-button').click()
-      cy.contains(testEmail)
+      cy.contains(testEmail1)
     })
   })
 
@@ -63,13 +64,13 @@ describe('auth' , () => {
       cy.visit('/')
       cy.getBySel('topbar-menu-button').click()
       cy.getBySel('sidebar-login-button').click()
-      cy.getBySel('auth-modal-email-input').type(testEmail)
-      cy.getBySel('auth-modal-password-input').type(testPassword)
+      cy.getBySel('auth-modal-email-input').type(testEmail1)
+      cy.getBySel('auth-modal-password-input').type(testPassword1)
       cy.getBySel('auth-modal-submit-button').click()
       cy.contains('Successfully logged in.')
       cy.getBySel('auth-modal-ok-button').click()
       cy.getBySel('topbar-menu-button').click()
-      cy.contains(testEmail)
+      cy.contains(testEmail1)
     })
 
     it('can be logged out', () => {
@@ -84,7 +85,7 @@ describe('auth' , () => {
     before(() => {
       cy.task('clearUser', testEmail2)
       cy.restoreLocalStorage()
-      cy.login(testEmail, testPassword)
+      cy.login(testEmail1, testPassword1)
       cy.saveLocalStorage()
     })
     beforeEach(() => {
@@ -105,8 +106,8 @@ describe('auth' , () => {
       cy.getBySel('auth-modal-ok-button').click()
       cy.getBySel('sidebar-logout-button').click()
       cy.getBySel('sidebar-login-button').click()
-      cy.getBySel('auth-modal-email-input').type(testEmail)
-      cy.getBySel('auth-modal-password-input').type(testPassword)
+      cy.getBySel('auth-modal-email-input').type(testEmail1)
+      cy.getBySel('auth-modal-password-input').type(testPassword1)
       cy.getBySel('auth-modal-submit-button').click()
       cy.contains('Email/Password is incorrect.')
       cy.getBySel('auth-modal-password-input').clear()
@@ -138,7 +139,7 @@ describe('auth' , () => {
     it('shows success message and gets logged in with new email when accessing sent url in html', () => {
       cy.visit(linkInMailChangeMail)
       cy.contains('Please enter your password to confirm new email.')
-      cy.getBySel('auth-modal-password-input').type(testPassword)
+      cy.getBySel('auth-modal-password-input').type(testPassword1)
       cy.getBySel('auth-modal-submit-button').click()
       cy.contains('Password is incorrect.')
       cy.getBySel('auth-modal-password-input').clear()
@@ -148,7 +149,7 @@ describe('auth' , () => {
       cy.getBySel('auth-modal-ok-button').click()
       cy.getBySel('topbar-menu-button').click()
       cy.contains(testEmail2)
-      cy.task('changeEmail', {from: testEmail2, to: testEmail})
+      cy.task('changeEmail', {from: testEmail2, to: testEmail1})
     })
 
     const mailWithPasswordChangeTestStart = new Date()
@@ -172,11 +173,11 @@ describe('auth' , () => {
         linkInMailWithPasswordChangeMail = message.html.links[0].href
       })
       cy.getBySel('auth-modal-ok-button').click()
-      cy.contains(testEmail)
+      cy.contains(testEmail1)
       cy.getBySel('sidebar-logout-button').click()
       cy.getBySel('sidebar-login-button').click()
       // old email/new password is correct here.
-      cy.getBySel('auth-modal-email-input').type(testEmail) // old email
+      cy.getBySel('auth-modal-email-input').type(testEmail1) // old email
       cy.getBySel('auth-modal-password-input').type(testPassword2) // old password
       cy.getBySel('auth-modal-submit-button').click()
       cy.contains('Email/Password is incorrect.')
@@ -187,7 +188,7 @@ describe('auth' , () => {
       cy.getBySel('auth-modal-submit-button').click()
       cy.contains('Email/Password is incorrect.')
       cy.getBySel('auth-modal-email-input').clear()
-      cy.getBySel('auth-modal-email-input').type(testEmail) // old email
+      cy.getBySel('auth-modal-email-input').type(testEmail1) // old email
       cy.getBySel('auth-modal-submit-button').click()
       cy.contains('Successfully logged in.')
     })
@@ -205,17 +206,73 @@ describe('auth' , () => {
       cy.getBySel('auth-modal-ok-button').click()
       cy.getBySel('topbar-menu-button').click()
       cy.contains(testEmail2)
-      cy.task('changeEmail', {from: testEmail2, to: testEmail})
+      cy.task('changeEmail', {from: testEmail2, to: testEmail1})
     })
   })
 
   context('reset password', () => {
-    it('sends confirmation message with url including token if password reset is requested', () => {
+    before(() => {
+      cy.restoreLocalStorage()
+      cy.logout()
+      cy.saveLocalStorage()
+    })
+    beforeEach(() => {
+      cy.restoreLocalStorage()
+    })
+    afterEach(() => {
+      cy.saveLocalStorage()
+    })
 
+    const passwordResetTestStart = new Date()
+    let linkInPasswordResetMail: string
+
+    it('sends confirmation message with url including token if password reset is requested', () => {
+      cy.visit('/')
+      cy.getBySel('topbar-menu-button').click()
+      cy.getBySel('sidebar-login-button').click()
+      cy.contains('Forgot password?').click()
+      cy.getBySel('auth-modal-email-input').type(testEmail1)
+      cy.getBySel('auth-modal-submit-button').click()
+      cy.contains(`Confirmation email was sent to ${testEmail1}.`)
+      cy.mailosaurGetMessage(
+        Cypress.env('MAILOSAUR_SERVER_ID'),
+        {sentTo: testEmail1, subject: 'Markdown: You asked to reset your password.'},
+        {receivedAfter: passwordResetTestStart}
+      ).then(message => {
+        cy.log(message.subject)
+        expect(message.html.links[0].href).to.equal(message.text.links[0].href)
+        linkInPasswordResetMail = message.html.links[0].href
+      })
     })
 
     it('shows success message and changes password when accessing sent url', () => {
-
+      cy.visit(linkInPasswordResetMail)
+      cy.getBySel('auth-modal-password-input').type(testPassword4)
+      cy.getBySel('auth-modal-password-confirm-input').type(testPassword4)
+      cy.getBySel('auth-modal-submit-button').click()
+      cy.contains('Successfully set new password.')
+      cy.getBySel('auth-modal-ok-button').click()
+      cy.getBySel('topbar-menu-button').click()
+      cy.getBySel('sidebar-logout-button').click()
+      cy.getBySel('sidebar-login-button').click()
+      cy.getBySel('auth-modal-email-input').type(testEmail1)
+      cy.getBySel('auth-modal-password-input').type(testPassword1)
+      cy.getBySel('auth-modal-submit-button').click()
+      cy.contains('Email/Password is incorrect.')
+      cy.getBySel('auth-modal-password-input').clear()
+      cy.getBySel('auth-modal-password-input').type(testPassword2)
+      cy.getBySel('auth-modal-submit-button').click()
+      cy.contains('Email/Password is incorrect.')
+      cy.getBySel('auth-modal-password-input').clear()
+      cy.getBySel('auth-modal-password-input').type(testPassword3)
+      cy.getBySel('auth-modal-submit-button').click()
+      cy.contains('Email/Password is incorrect.')
+      cy.getBySel('auth-modal-password-input').clear()
+      cy.getBySel('auth-modal-password-input').type(testPassword4)
+      cy.getBySel('auth-modal-submit-button').click()
+      cy.contains('Successfully logged in.')
+      cy.getBySel('auth-modal-ok-button').click()
+      cy.contains(testEmail1)
     })
   })
 
