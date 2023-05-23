0f8c28475eda7d8e793802c27a4d379eabcf7713
Takuma Kira <takuma@kirakiraworx.com>
2023-05-23 14:40:03 +0900

    WIP: [dev] Refine global test variable definitions

--- api/tests/@types/index.d.ts
+++ api/tests/@types/index.d.ts
@@ -5,12 +5,12 @@ import type { Server } from 'socket.io'
 import type getClient from '../../src/services/sessionStorage/getClient'
 
 declare global {
-  var apiApp: Express | undefined
-  var wsServer: Server | undefined
-  var db: ConnectionPool | undefined
+  var apiApp: Express
+  var wsServer: Server
+  var db: ConnectionPool
   /** When sharing connection pool above, sql also needs to be shared by unknown reason otherwise db.query function doesn't accept query built by sql function. */
-  var sql: SQL | undefined
-  var redisClient: ReturnType<typeof getClient>['client'] | undefined
+  var sql: SQL
+  var redisClient: ReturnType<typeof getClient>['client']
 }
 
 export {}
