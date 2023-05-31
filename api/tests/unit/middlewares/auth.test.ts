6557025e573fec8af9de153f9ae17db33e6d2a3d
Takuma Kira <takuma@kirakiraworx.com>
2023-06-01 00:37:40 +0900

    WIP: [fix] Fix auth middleware tests

--- api/tests/unit/middlewares/auth.test.ts
+++ api/tests/unit/middlewares/auth.test.ts
@@ -1,289 +1,108 @@
 import { Request, Response } from 'express'
-import jwt from 'jsonwebtoken'
 import { Socket } from 'socket.io'
-import { JWT_SECRET_KEY } from '../../../src/getEnvs'
-import { apiAuthMiddleware, wsAuthMiddleware } from "../../../src/middlewares/auth"
+import uid from 'uid-safe'
+import { getApiAuthMiddleware, getWsAuthMiddleware } from "../../../src/middlewares/auth"
+import { sessionStorageClientForTest } from '../../utils'
+import { getRedisKeyName } from '../../../src/services/sessionStorage'
+import { REDIS_KEYS } from '../../../src/constants'
+
+const validSession = {
+  sessionId: uid.sync(24),
+  wsHandshakeToken: uid.sync(24),
+  session: {
+    userId: '1'
+  }
+}
+
+beforeAll(async () => {
+  await sessionStorageClientForTest.setup()
+  await Promise.all([
+    sessionStorageClient.set(getRedisKeyName(REDIS_KEYS.SESSION, validSession.sessionId), JSON.stringify(validSession.session)),
+    sessionStorageClient.set(getRedisKeyName(REDIS_KEYS.WS_HANDSHAKE_TOKEN, validSession.wsHandshakeToken), validSession.sessionId),
+  ])
+})
+afterAll(async () => {
+  await sessionStorageClientForTest.close()
+  await new Promise(setImmediate)
+})
 
 describe('apiAuthMiddleware', () => {
-  // requests with valid token
-  test('triggers next() with validated user info if passed valid token', () => {
+  // requests with valid session
+  test('triggers next() if passed valid session info by session middleware', () => {
     const userId = 1
-    const userEmail = 'test@email.com'
-    const token = jwt.sign(
-      {is: 'AuthToken', id: userId, email: userEmail},
-      JWT_SECRET_KEY
-    )
-    const header = jest.fn().mockReturnValue(token)
-    const req = {header} as unknown as Request
+    const req = {session: {userId}} as unknown as Request
     const send = jest.fn()
     const status = jest.fn().mockReturnValue({send})
     const res = {status} as unknown as Response
     const next = jest.fn()
-    apiAuthMiddleware(req, res, next)
+    getApiAuthMiddleware()(req, res, next)
     expect(status).not.toBeCalled()
     expect(send).not.toBeCalled()
     expect(next).toBeCalledWith()
-    expect(req.user).toEqual({id: userId, email: userEmail, is: 'AuthToken', iat: expect.any(Number)})
   })
 
   // requests with invalid token
-  test('triggers status() and send() with error message if no auth token provided', () => {
-    const header = jest.fn().mockReturnValue(undefined)
-    const req = {header} as unknown as Request
+  test('triggers status() and send() with error message if no valid session provided by session middleware', () => {
+    const req = {session: {}} as unknown as Request
     const send = jest.fn()
     const status = jest.fn().mockReturnValue({send})
     const res = {status} as unknown as Response
     const next = jest.fn()
-    apiAuthMiddleware(req, res, next)
+    getApiAuthMiddleware()(req, res, next)
     expect(status).toBeCalledWith(401)
-    expect(send).toBeCalledWith({message: 'Access denied. Request does not have valid token.'})
+    expect(send).toBeCalledWith({message: 'Access denied. Request is not authorized.'})
     expect(next).not.toBeCalled()
-    expect(req.user).toEqual(undefined)
-  })
-
-  test('triggers status() and send() with error message if auth token is not valid', () => {
-    const userId = 1
-    const userEmail = 'test@email.com'
-    const token = jwt.sign(
-      {is: 'AuthToken', id: userId, email: userEmail},
-      JWT_SECRET_KEY
-    )
-    const header = jest.fn().mockReturnValue(`${token}1`)
-    const req = {header} as unknown as Request
-    const send = jest.fn()
-    const status = jest.fn().mockReturnValue({send})
-    const res = {status} as unknown as Response
-    const next = jest.fn()
-    apiAuthMiddleware(req, res, next)
-    expect(status).toBeCalledWith(401)
-    expect(send).toBeCalledWith({message: 'Access denied. Request does not have valid token.'})
-    expect(next).not.toBeCalled()
-    expect(req.user).toEqual(undefined)
-  })
-
-  test('triggers status() and send() with error message if given token is not encoded with right secret', () => {
-    const userId = 1
-    const userEmail = 'test@email.com'
-    const invalidToken = jwt.sign(
-      {is: 'AuthToken', id: userId, email: userEmail},
-      `${JWT_SECRET_KEY}1`
-    )
-    const header = jest.fn().mockReturnValue(invalidToken)
-    const req = {header} as unknown as Request
-    const send = jest.fn()
-    const status = jest.fn().mockReturnValue({send})
-    const res = {status} as unknown as Response
-    const next = jest.fn()
-    apiAuthMiddleware(req, res, next)
-    expect(status).toBeCalledWith(401)
-    expect(send).toBeCalledWith({message: 'Access denied. Request does not have valid token.'})
-    expect(next).not.toBeCalled()
-    expect(req.user).toEqual(undefined)
-  })
-
-  test('triggers status() and send() with error message if given token is not authToken', () => {
-    const userId = 1
-    const userEmail = 'test@email.com'
-    const notAuthToken = jwt.sign(
-      {is: 'SignupToken', id: userId, email: userEmail},
-      JWT_SECRET_KEY
-    )
-    const header = jest.fn().mockReturnValue(notAuthToken)
-    const req = {header} as unknown as Request
-    const send = jest.fn()
-    const status = jest.fn().mockReturnValue({send})
-    const res = {status} as unknown as Response
-    const next = jest.fn()
-    apiAuthMiddleware(req, res, next)
-    expect(status).toBeCalledWith(401)
-    expect(send).toBeCalledWith({message: 'Access denied. Request does not have valid token.'})
-    expect(next).not.toBeCalled()
-    expect(req.user).toEqual(undefined)
-  })
-
-  test('triggers status() and send() with error message if given token has too long email', () => {
-    const userId = 1
-    const tooLongUserEmail = 'abcdefghijabcdefghijabcdefghijabcdefghi@abcdefg.com'
-    const authTokenWithTooLongEmail = jwt.sign(
-      {is: 'AuthToken', id: userId, email: tooLongUserEmail},
-      JWT_SECRET_KEY
-    )
-    const header = jest.fn().mockReturnValue(authTokenWithTooLongEmail)
-    const req = {header} as unknown as Request
-    const send = jest.fn()
-    const status = jest.fn().mockReturnValue({send})
-    const res = {status} as unknown as Response
-    const next = jest.fn()
-    apiAuthMiddleware(req, res, next)
-    expect(status).toBeCalledWith(401)
-    expect(send).toBeCalledWith({message: 'Access denied. Request does not have valid token.'})
-    expect(next).not.toBeCalled()
-    expect(req.user).toEqual(undefined)
-  })
-
-  test('triggers next() with validated user info if passed valid token with not too long email', () => {
-    const userId = 1
-    const notTooLongUserEmail = 'abcdefghijabcdefghijabcdefghijabcdefghi@abcdef.com'
-    const token = jwt.sign(
-      {is: 'AuthToken', id: userId, email: notTooLongUserEmail},
-      JWT_SECRET_KEY
-    )
-    const header = jest.fn().mockReturnValue(token)
-    const req = {header} as unknown as Request
-    const send = jest.fn()
-    const status = jest.fn().mockReturnValue({send})
-    const res = {status} as unknown as Response
-    const next = jest.fn()
-    apiAuthMiddleware(req, res, next)
-    expect(status).not.toBeCalled()
-    expect(send).not.toBeCalled()
-    expect(next).toBeCalledWith()
-    expect(req.user).toEqual({id: userId, email: notTooLongUserEmail, is: 'AuthToken', iat: expect.any(Number)})
-  })
-
-  test('triggers status() and send() with error message if given token has invalid email', () => {
-    const userId = 1
-    const invalidUserEmail = 'invalid@'
-    const authTokenWithInvalidEmail = jwt.sign(
-      {is: 'AuthToken', id: userId, email: invalidUserEmail},
-      JWT_SECRET_KEY
-    )
-    const header = jest.fn().mockReturnValue(authTokenWithInvalidEmail)
-    const req = {header} as unknown as Request
-    const send = jest.fn()
-    const status = jest.fn().mockReturnValue({send})
-    const res = {status} as unknown as Response
-    const next = jest.fn()
-    apiAuthMiddleware(req, res, next)
-    expect(status).toBeCalledWith(401)
-    expect(send).toBeCalledWith({message: 'Access denied. Request does not have valid token.'})
-    expect(next).not.toBeCalled()
-    expect(req.user).toEqual(undefined)
   })
 })
 
 describe('wsAuthMiddleware', () => {
   // requests with valid token
-  test('triggers next() with validated user info and socket.join() if passed valid token', () => {
-    const userId = 1
-    const userEmail = 'test@email.com'
-    const token = jwt.sign(
-      {is: 'AuthToken', id: userId, email: userEmail},
-      JWT_SECRET_KEY
-    )
+  test('triggers next() with validated user info and socket.join() if passed valid wsHandshakeToken', async () => {
     const next = jest.fn()
     const join = jest.fn()
-    const socket = { handshake: { auth: { token } }, join } as unknown as Socket
-    wsAuthMiddleware(socket, next)
+    const close = jest.fn()
+    const socket = {
+      handshake: { auth: { wsHandshakeToken: validSession.wsHandshakeToken } },
+      join,
+      conn: { close }
+    } as unknown as Socket
+    await getWsAuthMiddleware(sessionStorageClient, sessionStorageClientIsReady)(socket, next)
     expect(next).toBeCalledWith()
-    expect(join).toBeCalledWith(userId.toString())
-    expect(socket.user).toEqual({id: userId, email: userEmail, is: 'AuthToken', iat: expect.any(Number)})
+    expect(join).toBeCalledWith(validSession.session.userId)
+    await new Promise(resolve => setTimeout(resolve, 10))
+    expect(close).not.toBeCalled()
   })
 
   // requests with invalid token
-  test('triggers next() with with error if no auth token provided', () => {
+  test('triggers next() with with error if no wsHandshakeToken provided', async () => {
     const next = jest.fn()
     const join = jest.fn()
-    const socket = { handshake: { auth: {} }, join } as unknown as Socket
-    wsAuthMiddleware(socket, next)
-    expect(next).toBeCalledWith(new Error('Access denied. Request does not have valid token.'))
+    const close = jest.fn()
+    const socket = {
+      handshake: { auth: {} },
+      join,
+      conn: { close }
+    } as unknown as Socket
+    await getWsAuthMiddleware(sessionStorageClient, sessionStorageClientIsReady)(socket, next)
+    expect(next).toBeCalledWith(new Error('Access denied. Handshake request does not have valid token.'))
     expect(join).not.toBeCalled()
-    expect(socket.user).toEqual(undefined)
+    await new Promise(resolve => setTimeout(resolve, 10))
+    expect(close).toBeCalled()
   })
 
-  test('triggers next() with with error if auth token is not valid', () => {
-    const userId = 1
-    const userEmail = 'test@email.com'
-    const token = jwt.sign(
-      {is: 'AuthToken', id: userId, email: userEmail},
-      JWT_SECRET_KEY
-    )
+  test('triggers next() with with error if wsHandshakeToken is not valid', async () => {
     const next = jest.fn()
     const join = jest.fn()
-    const socket = { handshake: { auth: { token: `${token}1` } }, join } as unknown as Socket
-    wsAuthMiddleware(socket, next)
-    expect(next).toBeCalledWith(new Error('Access denied. Request does not have valid token.'))
+    const close = jest.fn()
+    const socket = {
+      handshake: { auth: { wsHandshakeToken: `${validSession.wsHandshakeToken}1` } },
+      join,
+      conn: { close }
+    } as unknown as Socket
+    await getWsAuthMiddleware(sessionStorageClient, sessionStorageClientIsReady)(socket, next)
+    expect(next).toBeCalledWith(new Error('Access denied. Handshake request does not have valid token.'))
     expect(join).not.toBeCalled()
-    expect(socket.user).toEqual(undefined)
-  })
-
-  test('triggers next() with with error if given token is not encoded with right secret', () => {
-    const userId = 1
-    const userEmail = 'test@email.com'
-    const invalidToken = jwt.sign(
-      {is: 'AuthToken', id: userId, email: userEmail},
-      `${JWT_SECRET_KEY}1`
-    )
-    const next = jest.fn()
-    const join = jest.fn()
-    const socket = { handshake: { auth: { token: invalidToken } }, join } as unknown as Socket
-    wsAuthMiddleware(socket, next)
-    expect(next).toBeCalledWith(new Error('Access denied. Request does not have valid token.'))
-    expect(join).not.toBeCalled()
-    expect(socket.user).toEqual(undefined)
-  })
-
-  test('triggers next() with with error if given token is not authToken', () => {
-    const userId = 1
-    const userEmail = 'test@email.com'
-    const notAuthToken = jwt.sign(
-      {is: 'SignupToken', id: userId, email: userEmail},
-      JWT_SECRET_KEY
-    )
-    const next = jest.fn()
-    const join = jest.fn()
-    const socket = { handshake: { auth: { token: notAuthToken } }, join } as unknown as Socket
-    wsAuthMiddleware(socket, next)
-    expect(next).toBeCalledWith(new Error('Access denied. Request does not have valid token.'))
-    expect(join).not.toBeCalled()
-    expect(socket.user).toEqual(undefined)
-  })
-
-  test('triggers next() with with error if given token has too long email', () => {
-    const userId = 1
-    const tooLongUserEmail = 'abcdefghijabcdefghijabcdefghijabcdefghi@abcdefg.com'
-    const authTokenWithTooLongUserMail = jwt.sign(
-      {is: 'AuthToken', id: userId, email: tooLongUserEmail},
-      JWT_SECRET_KEY
-    )
-    const next = jest.fn()
-    const join = jest.fn()
-    const socket = { handshake: { auth: { token: authTokenWithTooLongUserMail } }, join } as unknown as Socket
-    wsAuthMiddleware(socket, next)
-    expect(next).toBeCalledWith(new Error('Access denied. Request does not have valid token.'))
-    expect(join).not.toBeCalled()
-    expect(socket.user).toEqual(undefined)
-  })
-
-  test('triggers next() with validated user info and socket.join() if passed valid token with not too long email', () => {
-    const userId = 1
-    const notTooLongUserEmail = 'abcdefghijabcdefghijabcdefghijabcdefghi@abcdef.com'
-    const token = jwt.sign(
-      {is: 'AuthToken', id: userId, email: notTooLongUserEmail},
-      JWT_SECRET_KEY
-    )
-    const next = jest.fn()
-    const join = jest.fn()
-    const socket = { handshake: { auth: { token } }, join } as unknown as Socket
-    wsAuthMiddleware(socket, next)
-    expect(next).toBeCalledWith()
-    expect(join).toBeCalledWith(userId.toString())
-    expect(socket.user).toEqual({id: userId, email: notTooLongUserEmail, is: 'AuthToken', iat: expect.any(Number)})
-  })
-
-  test('triggers next() with with error if given token has invalid email', () => {
-    const userId = 1
-    const invalidUserEmail = 'invalid@'
-    const authTokenWithInvalidUserMail = jwt.sign(
-      {is: 'AuthToken', id: userId, email: invalidUserEmail},
-      JWT_SECRET_KEY
-    )
-    const next = jest.fn()
-    const join = jest.fn()
-    const socket = { handshake: { auth: { token: authTokenWithInvalidUserMail } }, join } as unknown as Socket
-    wsAuthMiddleware(socket, next)
-    expect(next).toBeCalledWith(new Error('Access denied. Request does not have valid token.'))
-    expect(join).not.toBeCalled()
-    expect(socket.user).toEqual(undefined)
+    await new Promise(resolve => setTimeout(resolve, 10))
+    expect(close).toBeCalled()
   })
 })
